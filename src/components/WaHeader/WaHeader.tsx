0147226b38709a0913369b7f524f86566d24654b
Juan David Rodriguez <juandavid.rodriguez0222@gmail.com>
2024-02-02 20:11:46 -0500

    feat: añadiendo componente InfoAditional

--- src/components/WaHeader/WaHeader.tsx
+++ src/components/WaHeader/WaHeader.tsx
@@ -1,15 +1,17 @@
+import style from './WaHeader.module.css'
+
 type WaHeaderProps = {
   name:string| undefined,
   time:string| undefined
 }
+
 const WaHeader = ({ name, time }:WaHeaderProps) => {
   return (
-    <header className='header'>
-      <div className='header__location'>
+    <header className={style.header}>
+      <div className={style.header__location}>
         <h3>{time}</h3>
         <h2>{name}</h2>
       </div>
-
     </header>
   )
 }
